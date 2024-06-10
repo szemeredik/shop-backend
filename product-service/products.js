7718037d2d447a203ea12a98150e5307eafa74f7
Krisztian Szemeredi <krisztian_szemeredi@epam.com>
2024-06-10 14:42:00 +0200

    Products updated, CORS fixed

--- product-service/products.js
+++ product-service/products.js
@@ -1,7 +1,60 @@
 const products = [
-  { id: 1, name: "Product 1", price: 10 },
-  { id: 2, name: "Product 2", price: 20 },
-  { id: 3, name: "Product 3", price: 30 },
+  {
+    count: 5,
+    description: "Short Product Description1 updated",
+    id: 1,
+    price: 3.5,
+    title: "ProductOne Updated",
+  },
+  {
+    count: 10,
+    description: "Short Product Description3 updated",
+    id: 2,
+    price: 12,
+    title: "ProductNew Updated",
+  },
+  {
+    count: 8,
+    description: "Short Product Description2 updated",
+    id: 3,
+    price: 25,
+    title: "ProductTop Updated",
+  },
+  {
+    count: 14,
+    description: "Short Product Description7 updated",
+    id: 4,
+    price: 18,
+    title: "ProductTitle Updated",
+  },
+  {
+    count: 9,
+    description: "Short Product Description2 updated",
+    id: 5,
+    price: 28,
+    title: "Product Updated",
+  },
+  {
+    count: 11,
+    description: "Short Product Description4 updated",
+    id: 6,
+    price: 18,
+    title: "ProductTest Updated",
+  },
+  {
+    count: 4,
+    description: "Short Product Description1 updated",
+    id: 7,
+    price: 26,
+    title: "Product2 Updated",
+  },
+  {
+    count: 6,
+    description: "Short Product Description7 updated",
+    id: 8,
+    price: 17,
+    title: "ProductName Updated",
+  },
 ];
 
 module.exports = products;
